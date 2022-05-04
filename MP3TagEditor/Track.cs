cad857a78c878011bbc90a30f7ecd0849bd598ca
Apocaloypto <51018969+Apocaloypto@users.noreply.github.com>
2022-05-04 20:08:05 +0200

    Dinge

--- MP3TagEditor/Track.cs
+++ MP3TagEditor/Track.cs
@@ -9,6 +9,8 @@
       public string Album { get; set; }
       public string NewFilename => Trackname + ".mp3";
 
+      private string _directory { get; }
+
       public Track(int no, FileInfo info)
       {
          ID3Reader.RelevantID3Tags metaId3 = ID3Reader.ReadMetadata(info.FullName);
@@ -18,6 +20,7 @@
          Trackname = metaId3.Title;
          Artist = metaId3.Artist;
          Album = metaId3.Album;
+         _directory = info.DirectoryName!;
       }
    }
 }
